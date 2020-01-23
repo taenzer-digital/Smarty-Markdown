c96b2d684a9022dfed6a1b360b00dcd80102f5fb
Daniel Crump <d.crump@taenzer.me>
2020-01-23 14:00:11 +0100

    removed class extension

--- function.markdown.php
+++ function.markdown.php
@@ -24,16 +24,13 @@ require_once 'vendor/autoload.php';
 
 use \Michelf\Markdown;
 
-class Smarty_Compiler_Markdown extends Smarty_Internal_CompileBase
+function smarty_function_markdown($params, &$smarty)
 {
-    public function smarty_function_markdown($params, &$smarty)
-    {
 
-        if (!in_array('text', array_keys($params))) {
-            trigger_error("markdown missing 'text' parameter", E_USER_NOTICE);
-            return;
-        }
-
-        return Markdown::defaultTransform($params['text']);
+    if (!in_array('text', array_keys($params))) {
+        trigger_error("markdown missing 'text' parameter", E_USER_NOTICE);
+        return;
     }
+
+    return Markdown::defaultTransform($params['text']);
 }
