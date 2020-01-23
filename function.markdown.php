f8d3aa1fe27d4effb6bf9f977f95e79194853c20
Daniel Crump <d.crump@taenzer.me>
2020-01-23 11:41:10 +0100

    added composer config

--- function.markdown.php
+++ function.markdown.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Smarty plugin
  * 
@@ -19,19 +20,20 @@
  * @return string with converted text
  */
 
-require_once('PHP-Markdown-Lib-1.2.7/Michelf/Markdown.php');
+require_once 'vendor/autoload.php';
 
 use \Michelf\Markdown;
 
-function smarty_function_markdown($params, &$smarty) {
+class Smarty_Compiler_Markdown extends Smarty_Internal_CompileBase
+{
+    public function smarty_function_markdown($params, &$smarty)
+    {
 
-	if (!in_array('text', array_keys($params))) {
-		trigger_error("markdown missing 'text' parameter", E_USER_NOTICE);
-        return;
+        if (!in_array('text', array_keys($params))) {
+            trigger_error("markdown missing 'text' parameter", E_USER_NOTICE);
+            return;
+        }
+
+        return Markdown::defaultTransform($params['text']);
     }
-    
-    return Markdown::defaultTransform($params['text']);
-
 }
-
-?>
